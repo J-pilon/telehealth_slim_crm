01be1484330c6eda5ade3ee47bf1b1df15938eb9
Josiah <josiah.pilon@gmail.com>
2025-10-16 14:48:52 -0700

    tests message policy for correct authorization

---
+++ spec/policies/message_policy_spec.rb
@@ -0,0 +1,75 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe MessagePolicy, type: :policy do
+  let(:admin) { create(:user, :admin) }
+  let(:patient_user) { create(:user, :patient) }
+  let(:other_patient) { create(:user, :patient) }
+  let(:patient_record) { create(:patient) }
+  let(:message) { create(:message, patient: patient_record, user: admin) }
+
+  describe '#index?' do
+    it 'allows logged in users' do
+      expect(MessagePolicy.new(admin, Message).index?).to be true
+      expect(MessagePolicy.new(patient_user, Message).index?).to be true
+    end
+  end
+
+  describe '#show?' do
+    it 'allows admins to view any message' do
+      expect(MessagePolicy.new(admin, message).show?).to be true
+    end
+
+    it 'allows patients to view messages' do
+      patient_message = create(:message, patient: patient_record, user: patient_user)
+      expect(MessagePolicy.new(patient_user, patient_message).show?).to be true
+    end
+  end
+
+  describe '#create?' do
+    it 'allows logged in users' do
+      expect(MessagePolicy.new(admin, Message).create?).to be true
+      expect(MessagePolicy.new(patient_user, Message).create?).to be true
+    end
+  end
+
+  describe '#update?' do
+    it 'allows admins to update any message' do
+      expect(MessagePolicy.new(admin, message).update?).to be true
+    end
+
+    it 'allows patients to update their own messages' do
+      patient_message = create(:message, patient: patient_record, user: patient_user)
+      expect(MessagePolicy.new(patient_user, patient_message).update?).to be true
+    end
+
+    it 'denies patients from updating other users messages' do
+      expect(MessagePolicy.new(patient_user, message).update?).to be false
+    end
+  end
+
+  describe '#destroy?' do
+    it 'allows admins' do
+      expect(MessagePolicy.new(admin, message).destroy?).to be true
+    end
+
+    it 'denies patients' do
+      expect(MessagePolicy.new(patient_user, message).destroy?).to be false
+    end
+  end
+
+  describe 'Scope' do
+    it 'returns all messages for admins' do
+      create_list(:message, 3)
+      expect(MessagePolicy::Scope.new(admin, Message).resolve.count).to eq(3)
+    end
+
+    it 'returns no messages for patients' do
+      create_list(:message, 2, patient: patient_record, user: patient_user)
+      create(:message, patient: create(:patient), user: admin)
+
+      expect(MessagePolicy::Scope.new(patient_user, Message).resolve.count).to eq(0)
+    end
+  end
+end
