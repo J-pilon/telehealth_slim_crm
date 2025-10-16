d9112eb64a7949cd46a1e834893712e58fc79586
Josiah <josiah.pilon@gmail.com>
2025-10-16 14:49:16 -0700

    tests task policy for correct authorization

---
+++ spec/policies/task_policy_spec.rb
@@ -0,0 +1,74 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe TaskPolicy, type: :policy do
+  let(:admin) { create(:user, :admin) }
+  let(:patient_user) { create(:user, :patient) }
+  let(:other_patient) { create(:user, :patient) }
+  let(:patient_record) { create(:patient) }
+  let(:task) { create(:task, patient: patient_record) }
+
+  describe '#index?' do
+    it 'allows logged in users' do
+      expect(TaskPolicy.new(admin, Task).index?).to be true
+      expect(TaskPolicy.new(patient_user, Task).index?).to be true
+    end
+  end
+
+  describe '#show?' do
+    it 'allows admins to view any task' do
+      expect(TaskPolicy.new(admin, task).show?).to be true
+    end
+
+    it 'allows patients to view tasks' do
+      patient_task = create(:task, patient: patient_record)
+      expect(TaskPolicy.new(patient_user, patient_task).show?).to be true
+    end
+  end
+
+  describe '#create?' do
+    it 'allows admins' do
+      expect(TaskPolicy.new(admin, Task).create?).to be true
+    end
+
+    it 'denies patients' do
+      expect(TaskPolicy.new(patient_user, Task).create?).to be false
+    end
+  end
+
+  describe '#update?' do
+    it 'allows admins to update any task' do
+      expect(TaskPolicy.new(admin, task).update?).to be true
+    end
+
+    it 'allows patients to update tasks' do
+      patient_task = create(:task, patient: patient_record)
+      expect(TaskPolicy.new(patient_user, patient_task).update?).to be true
+    end
+  end
+
+  describe '#destroy?' do
+    it 'allows admins' do
+      expect(TaskPolicy.new(admin, task).destroy?).to be true
+    end
+
+    it 'denies patients' do
+      expect(TaskPolicy.new(patient_user, task).destroy?).to be false
+    end
+  end
+
+  describe 'Scope' do
+    it 'returns all tasks for admins' do
+      create_list(:task, 3)
+      expect(TaskPolicy::Scope.new(admin, Task).resolve.count).to eq(3)
+    end
+
+    it 'returns no tasks for patients' do
+      create_list(:task, 2, patient: patient_record)
+      create(:task, patient: create(:patient))
+
+      expect(TaskPolicy::Scope.new(patient_user, Task).resolve.count).to eq(0)
+    end
+  end
+end
