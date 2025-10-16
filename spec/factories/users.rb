2233285e1f0a2e6f8b572f101a69b3ac0c35f286
Josiah <josiah.pilon@gmail.com>
2025-10-16 13:58:09 -0700

    adds user factory

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,5 +1,16 @@
 FactoryBot.define do
   factory :user do
-    
+    email { Faker::Internet.email }
+    password { "password123" }
+    password_confirmation { "password123" }
+    role { "admin" }
+
+    trait :admin do
+      role { "admin" }
+    end
+
+    trait :patient do
+      role { "patient" }
+    end
   end
 end
